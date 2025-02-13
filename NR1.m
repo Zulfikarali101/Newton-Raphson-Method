6d9e312bda1e9bbaadb6327b1d29cf6e3d782b45
Zulfikarali101 <zulfikar.ibadurrahman@gmail.com>
2025-02-13 12:25:51 +0700

    Add files via upload

---
+++ NR1.m
@@ -0,0 +1,20 @@
+%Metode Newton-Raphson
+%Zulfikar Ali Ibadurrahman
+%12918030
+
+a=input('Masukkan nilai Xn: ');
+err=input('Masukkan nilai toleransi: ');
+e=1;
+i=0;
+
+while e>=err
+    fa=(0.49*(10^(1/2))*(a^(1/2)))-8;
+    faksen=(0.49*(10^(1/2))*(1/2)*(a^-(1/2)));
+    i=i+1;
+    b=a-(fa/faksen);
+    e=abs(b-a);
+    a=b;
+end
+
+disp(['Solusi: ',num2str(b)])
+disp(['iterasi: ',num2str(i)])
